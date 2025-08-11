0cdca2deac6d3b53e0ef6afbae9b41edb795bf2e
chacha86 <taejincha0619@gmail.com>
2025-08-11 11:34:55 +0900

    exp2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,38 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        experiment1();
+//        experiment1();
+        experiment2();
+    }
+
+    public static void experiment2() {
+        System.out.println("안녕하세요");
+
+        PrintStream originalOut = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        System.setOut(printStream);
+
+        System.out.println("하하하");
+
+        String outStr = outputStream.toString();
+        System.setOut(originalOut);
+        printStream.close();
+
+        if(outStr.equals("하하하")) {
+            System.out.println("출력 결과가 일치합니다.");
+        } else {
+            System.out.println("출력 결과가 일치하지 않습니다.");
+        }
+
+        System.out.println(outStr);
+
+
     }
 
     public static void experiment1() {
