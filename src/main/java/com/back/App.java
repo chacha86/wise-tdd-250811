8642354a4ded970a0edaef5dbef602481ca535bc
chacha86 <taejincha0619@gmail.com>
2025-08-11 14:37:19 +0900

    step4 - green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -13,6 +13,8 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        int lastId = 0;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine();
@@ -24,7 +26,9 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
 
-                    System.out.println("1번 명언이 등록되었습니다.");
+                    lastId++;
+
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
                 }
                 case "종료" -> {
                     return;
