511f615c453d642676bb31d204bb9dbdd331ceca
chacha86 <taejincha0619@gmail.com>
2025-08-14 15:16:58 +0900

    step28 red

--- src/test/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -4,6 +4,7 @@ import com.back.AppConfig;
 import com.back.AppContext;
 import com.back.PageDto;
 import com.back.domain.wiseSaying.entity.WiseSaying;
+import com.back.standard.util.Util;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
@@ -196,6 +197,22 @@ public class WiseSayingFileRepositoryTest {
                         wiseSaying3,
                         wiseSaying1
                 );
+
+    }
+
+    @Test
+    @DisplayName("빌드 - data.json 파일이 생성된다.")
+    void t9() {
+
+        WiseSaying wiseSaying1 = new WiseSaying("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingFileRepository.save(wiseSaying1);
+
+        WiseSaying wiseSaying2 = new WiseSaying("너 자신을 알라.", "소크라테스");
+        wiseSayingFileRepository.save(wiseSaying2);
+
+        String filePath = wiseSayingFileRepository.build();
+
+        assertThat(Util.file.exists(filePath)).isTrue();
 
     }
 }
